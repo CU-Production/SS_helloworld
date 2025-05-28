d16990a9b25cd8755cc090f201759269460fb01f
THISISAGOODNAME <yangyj19931231@gmail.com>
2025-05-28 11:33:29 +0800

    Add 3 planes demo

--- main.c
+++ main.c
@@ -1,37 +1,9 @@
-/*
-** Jo Sega Saturn Engine
-** Copyright (c) 2012-2021, Johannes Fetz (johannesfetz@gmail.com)
-** All rights reserved.
-**
-** Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are met:
-**     * Redistributions of source code must retain the above copyright
-**       notice, this list of conditions and the following disclaimer.
-**     * Redistributions in binary form must reproduce the above copyright
-**       notice, this list of conditions and the following disclaimer in the
-**       documentation and/or other materials provided with the distribution.
-**     * Neither the name of the Johannes Fetz nor the
-**       names of its contributors may be used to endorse or promote products
-**       derived from this software without specific prior written permission.
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-** DISCLAIMED. IN NO EVENT SHALL Johannes Fetz BE LIABLE FOR ANY
-** DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-** ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #include <jo/jo.h>
 
-jo_camera               cam;
-jo_3d_mesh              *mesh;
+jo_camera   cam;
+jo_3d_mesh* mesh;
 
-void			    my_draw(void)
+void my_draw(void)
 {
     static short    rx, ry, rz;
 
@@ -51,34 +23,41 @@ void			    my_draw(void)
     jo_printf(0, 28, "Polygon count: %d  ", jo_3d_get_polygon_count());
 }
 
-void			jo_main(void)
+void jo_main(void)
 {
-	jo_core_init(JO_COLOR_Black);
+  jo_core_init(JO_COLOR_Black);
     jo_3d_camera_init(&cam);
 
     // We create programmatically a mesh composed of 2 quads
-    mesh = jo_3d_create_mesh(2);
+    mesh = jo_3d_create_mesh(3);
     // Note : There is also jo_3d_create_mesh_from_vertices() and jo_3d_create_mesh_from_vertices_and_normals()
     // We fill in the first 4 vertices of the first quad (index 0 to 3)
     jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(-50), jo_int2fixed(0), 0);
-    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(50), jo_int2fixed(-50), jo_int2fixed(0), 1);
-    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(50), jo_int2fixed(50), jo_int2fixed(0), 2);
-    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(50), jo_int2fixed(0), 3);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed( 50), jo_int2fixed(-50), jo_int2fixed(0), 1);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed( 50), jo_int2fixed( 50), jo_int2fixed(0), 2);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed( 50), jo_int2fixed(0), 3);
     // We fill in the first 4 vertices of the second quad (index 4 to 7)
-    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(-50), jo_int2fixed(-50), 4);
-    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(50), jo_int2fixed(-50), 5);
-    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(50), jo_int2fixed(50), 6);
-    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(-50), jo_int2fixed(50), 7);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(0), jo_int2fixed(-50), jo_int2fixed(-50), 4);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(0), jo_int2fixed( 50), jo_int2fixed(-50), 5);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(0), jo_int2fixed( 50), jo_int2fixed( 50), 6);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(0), jo_int2fixed(-50), jo_int2fixed( 50), 7);
+    // We fill in the first 4 vertices of the third quad (index 8 to 11)
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(0), jo_int2fixed(-50),  8);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed( 50), jo_int2fixed(0), jo_int2fixed(-50),  9);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed( 50), jo_int2fixed(0), jo_int2fixed( 50), 10);
+    jo_3d_set_mesh_vertice(mesh, jo_int2fixed(-50), jo_int2fixed(0), jo_int2fixed( 50), 11);
     // Note: If the coordinates of the two last vertices in a quad are identical, this will produce a triangle
     // We set the color of the first quad (index 0)
-    jo_3d_set_mesh_polygon_color(mesh, JO_COLOR_Yellow, 0);
+    jo_3d_set_mesh_polygon_color(mesh, JO_COLOR_Red, 0);
     // We set the color of the second quad (index 1)
-    jo_3d_set_mesh_polygon_color(mesh, JO_COLOR_Cyan, 1);
+    jo_3d_set_mesh_polygon_color(mesh, JO_COLOR_Green, 1);
+    // We set the color of the second quad (index 2)
+    jo_3d_set_mesh_polygon_color(mesh, JO_COLOR_Blue, 2);
     // Note: of course, you can also use other methods like jo_3d_set_mesh_polygon_transparency(), jo_3d_set_mesh_polygon_texture(), etc
     // Warning: you can experience some glitch. this is because on have to set other attributes in mesh like sorting and normals
 
-	jo_core_add_callback(my_draw);
-	jo_core_run();
+  jo_core_add_callback(my_draw);
+  jo_core_run();
 }
 
 /*
